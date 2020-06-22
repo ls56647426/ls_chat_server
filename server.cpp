0c4c403c421ac7c91928c61d508e928821de48bb
longshao <ls56647426@foxmail.com>
2020-06-23 06:45:51 +0800

    server

---
+++ server.cpp
@@ -0,0 +1,41 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  server.cpp
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  06/19/2020 11:27:31 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+/* c std lib */
+#include <stdlib.h>
+
+/* third-party lib */
+#include <event2/listener.h>
+#include <event2/bufferevent.h>
+#include <event2/buffer.h>
+#include <event2/event.h>
+
+#include "server.h"
+
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  main
+ *  Description:  
+ * =====================================================================================
+ */
+int main ( int argc, char *argv[] )
+{
+
+	return EXIT_SUCCESS;
+}
+
---
+++ server.h
@@ -0,0 +1,25 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  server.h
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  06/19/2020 01:36:54 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#ifndef _SERVER_H_
+#define _SERVER_H_
+
+
+
+#endif
+
